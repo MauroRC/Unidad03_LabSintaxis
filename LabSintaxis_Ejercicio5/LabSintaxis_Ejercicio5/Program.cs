f2fba4d91b4bb9bbdc32307da01fdf0e9972196c
Mauro Gimenez <mauro88rc@gmail.com>
2015-04-22 16:36:24 -0300

    9no commit: Ejercicio5

---
+++ LabSintaxis_Ejercicio5/LabSintaxis_Ejercicio5/Program.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LabSintaxis_Ejercicio5
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string[] meses = new string[] {"enero", "febrero", "marzo", "abril", "mayo", "junio", "julio", "agosto", "setiembre", "octubre", 
+                "noviembre", "diciembre"};
+            string mes;
+            bool aux = false;
+            Console.Write("Ingrese nombre del mes: ");
+            mes= Console.ReadLine().ToLower();
+            for (int i = 1; i <= meses.Length; i++)
+            {
+                if (mes == meses[i-1])
+                {
+                    Console.WriteLine("\nEl nro de mes es: " + i);
+                    aux = true;
+                }
+            }
+            if (aux == false)
+            {
+                Console.WriteLine("\nEl nombre ingresado no corresponde a ningun mes valido.");
+            }
+            Console.WriteLine();
+        }
+    }
+}
