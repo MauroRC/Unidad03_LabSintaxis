2bf32a2bab1e3bbc1d4ed619df11ee24ae3c978b
Mauro Gimenez <mauro88rc@gmail.com>
2015-04-21 23:35:52 -0300

    5to commit: Ejercicio1

---
+++ LabSintaxis_Ejercicio1/LabSintaxis_Ejercicio1/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LabSintaxis_Ejercicio1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            try
+            {
+                int nro1, nro2, rta;
+                Console.Write("\nIngrese 1er nro: ");
+                nro1 = int.Parse(Console.ReadLine());
+                Console.Write("\nIngrese 2er nro: ");
+                nro2 = int.Parse(Console.ReadLine());
+                rta= nro1 + nro2;
+                Console.WriteLine("\nEl resultado de la suma de " + nro1 + " y " + nro2 + " es " + rta);
+            }
+            catch (FormatException)
+            {
+                Console.WriteLine("\nERROR: Debe ingresar un numero!!!");
+            }
+        }
+    }
+}
