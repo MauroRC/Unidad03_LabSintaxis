d93912cde3abfdc44e2f533da6e0e5e15b935b0f
Mauro Gimenez <mauro88rc@gmail.com>
2015-04-22 08:16:12 -0300

    7mo commit: Ejercicio3

---
+++ LabSintaxis_Ejercicio2/LabSintaxis_Ejercicio2/Program.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LabSintaxis_Ejercicio2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            try
+            {
+                int anio;
+                Console.Write("Ingrese un año (debe tener entre 1 y 4 digitos): ");
+                anio = int.Parse(Console.ReadLine());
+                if (anio > 0 & anio < 10000)
+                {
+                    if (anio % 100 == 0)
+                    {
+                        if (anio % 400 == 0)
+                        {
+                            Console.WriteLine("\nAño bisiesto.");
+                        }
+                        else
+                        {
+                            Console.WriteLine("\nAño no bisiesto.");
+                        }
+                    }
+                    else if (anio % 4 == 0)
+                    {
+                        Console.WriteLine("\nAño bisiesto.");
+                    }
+                    else
+                    {
+                        Console.WriteLine("\nAño no bisiesto.");
+                    }
+
+                }
+                else
+                {
+                    Console.WriteLine("\nEl año se encuentra fuera del rango establecido.");
+                }
+            }
+            catch (FormatException)
+            {
+                Console.WriteLine("\nERROR: El dato ingresado debe ser un numero");
+            }
+        }
+    }
+}
---
+++ LabSintaxis_Ejercicio3/LabSintaxis_Ejercicio3/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LabSintaxis_Ejercicio3
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int limite = 20;
+            int var1 = 0;
+            int var2 = 1;
+            int suma=0;
+            Console.Write(var1);
+            for (int i = 0; i < limite; i++)
+            {
+                Console.Write("," + var2);
+                suma = var1 + var2;
+                var1 = var2;
+                var2 = suma;
+            }
+            Console.WriteLine();
+        }
+    }
+}
