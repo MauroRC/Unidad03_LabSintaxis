b21fb39ee90eede2084a5ffe864bd954dcf926f6
Mauro Gimenez <mauro88rc@gmail.com>
2015-04-22 16:59:16 -0300

    10mo commit: ejercicio8

---
+++ LabSintaxis_Ejercicio8/LabSintaxis_Ejercicio8/Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LabSintaxis_Ejercicio8
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int maximoIntentos = 3;
+            string clave = "1234";
+            string rta;
+            for (int i = 0; i < maximoIntentos; i++)
+			{
+                Console.Write("Introduzca clave: ");
+                rta = Console.ReadLine();
+                if (rta== clave)
+                {
+                    Console.WriteLine("\nClave correcta !!!\n");
+                    break;
+                }
+                else
+                {
+                    Console.WriteLine("\nClave incorrecta\n");
+                }
+			}
+            Console.WriteLine("\nFIN DE LA APLICACION\n");
+        }
+    }
+}
