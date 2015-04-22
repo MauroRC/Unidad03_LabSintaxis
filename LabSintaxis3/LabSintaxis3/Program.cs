03b1e8fa37f2dde2f6f6eb0a57276041dfc95acf
Mauro Gimenez <mauro88rc@gmail.com>
2015-04-21 22:18:56 -0300

    4to commit: LabSintaxis3

---
+++ LabSintaxis3/LabSintaxis3/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LabSintaxis3
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int cantIteraciones =5;
+            string [] arrayDeString = new string [cantIteraciones];
+
+            Console.WriteLine("Ingrese " + cantIteraciones + " strings");
+            for (int i = 1; i < arrayDeString.Length + 1; i++)
+            {
+                Console.Write("\nIngrese el " + i + "º string: ");
+                arrayDeString[i - 1] = Console.ReadLine();
+            }
+            Console.WriteLine("\nStrings ingresados ordenados del ultimo al primero ingresados:");
+            for (int i = arrayDeString.Length - 1; i >=0; i--)
+            {
+                Console.WriteLine(arrayDeString[i]);
+            }
+        }
+    }
+}
