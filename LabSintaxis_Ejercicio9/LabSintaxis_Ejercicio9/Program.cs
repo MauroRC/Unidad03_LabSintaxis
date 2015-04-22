b4d98ca3b588bc1c2018cf56b9ec522127d2f9e4
Mauro Gimenez <mauro88rc@gmail.com>
2015-04-22 17:27:09 -0300

    11vo commit: Ejercicio9

---
+++ LabSintaxis_Ejercicio9/LabSintaxis_Ejercicio9/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LabSintaxis_Ejercicio9
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int filas;
+            Console.Write("Ingrese cantidad de filas: ");
+            filas = int.Parse(Console.ReadLine());
+            for (int i = 0; i < filas; i++)
+            {
+                for (int j = 0; j < (filas-i); j++)
+                {
+                    Console.Write(" ");
+                }
+                for (int j = 0; j < (i*2 +1); j++)
+                {
+                    Console.Write("*");
+                }
+                Console.WriteLine();
+            }
+        }
+    }
+}
