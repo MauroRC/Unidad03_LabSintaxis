46c8408fe7028ce163ba80baaaaaee35ffbb9108
Mauro Gimenez <mauro88rc@gmail.com>
2015-04-22 16:07:21 -0300

    8vo commit: Ejercicio4

---
+++ LabSintaxis_Ejercicio4/LabSintaxis_Ejercicio4/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LabSintaxis_Ejercicio4
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            for (int i = 1; i <= 100; i++)
+            {
+                if (i%2==0)
+                {
+                    Console.Write(i + " ");
+                }
+            }
+            Console.WriteLine();
+        }
+    }
+}
