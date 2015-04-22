85f6ab5a3ce82168ade157c5e753d635a90db32a
Mauro Gimenez <mauro88rc@gmail.com>
2015-04-21 21:45:49 -0300

    2do commit: LabSintaxis1

--- LabSintaxis1/LabSintaxis1/Program.cs
+++ LabSintaxis1/LabSintaxis1/Program.cs
@@ -10,6 +10,35 @@ namespace LabSintaxis1
     {
         static void Main(string[] args)
         {
+            string inputTexto;
+            Console.Write("Ingrese texto: ");
+            inputTexto = Console.ReadLine();
+            if (inputTexto.Length != 0)
+            {
+                Console.Write("\nMENU:\n1- Mostrar texto en MAYUSCULA.\n2- Mostrar texto en minuscula. \n3- Mostrar cantidad de caracteres.\nRespuesta: ");
+                ConsoleKeyInfo opcion = Console.ReadKey();
+                if (opcion.Key == ConsoleKey.D1 )
+                {
+                    Console.WriteLine("\n\n"+inputTexto.ToUpper());
+                }
+                else if (opcion.Key == ConsoleKey.D2)
+                {
+                    Console.WriteLine("\n\n" + inputTexto.ToLower());
+                }
+                else if (opcion.Key == ConsoleKey.D3)
+                {
+                    Console.WriteLine("\n\nCantidad de caracteres: " + inputTexto.Length);
+                }
+                else
+                {
+                    Console.WriteLine("\nHA INGRESADO UNA OPCION NO VALIDA.");
+                }
+            }
+            else
+            {
+                Console.WriteLine("\nNO HA INGRESADO NINGUN TEXTO.");
+            }
+            Console.WriteLine("\nFIN DE LA APLICACION");  
         }
     }
 }
